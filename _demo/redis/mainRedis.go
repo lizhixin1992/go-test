fc32674b3403645481b4a7ed4babba16f8530704
lizhixin <lzx920911@gmail.com>
2019-04-30 11:21:20 +0800

    commit

--- _demo/redis/mainRedis.go
+++ _demo/redis/mainRedis.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/go-redis/redis"
 )
 
 func main() {
 	//单机
 	client := redis.NewClient(&redis.Options{
-		Addr:         "127.0.0.1:6379",
-		Password:     "",
-		DB:           0,
-		PoolSize:     5,
-		MinIdleConns: 3,
+		Addr:     "127.0.0.1:6379",
+		Password: "",
+		DB:       0,
 	})
 	defer client.Close()
 	//fmt.Println(client.Get("redisUtil"))
@@ -20,20 +17,51 @@ func main() {
 
 	//wg := sync.WaitGroup{}
 	//wg.Add(10)
-
+	//
 	//for i := 0; i < 10; i++ {
 	//	go func() {
 	//		defer wg.Done()
-
-	for j := 0; j < 100; j++ {
-		client.Set(fmt.Sprintf("name%d", j), fmt.Sprintf("xys%d", j), 0).Err()
-		client.Get(fmt.Sprintf("name%d", j)).Result()
-	}
-
-	fmt.Println("PoolStats:", client.PoolStats())
-	//}()
+	//
+	//		for j := 0; j < 100; j++ {
+	//			client.Set(fmt.Sprintf("name%d", j), fmt.Sprintf("xys%d", j), 0).Err()
+	//			client.Get(fmt.Sprintf("name%d", j)).Result()
+	//		}
+	//
+	//		fmt.Println("PoolStats:", client.PoolStats())
+	//	}()
 	//}
-
+	//
 	//wg.Wait()
 
+	//client.Set("test","test11111",30 * time.Second)
+	//fmt.Println("PoolStats:", client.PoolStats())
+
+	//v,err := client.Get("test").Result()
+	//fmt.Println(v,err)
+
+	//commons.SetString("test","test11111",30 * time.Second)
+	//fmt.Println(commons.GetString("test"))
+
+	//fmt.Println(client.SetNX("test1","test11111",30 * time.Second).Result())
+	//fmt.Println(commons.SetNXString("test1","test11111",30 * time.Second))
+
+	//fmt.Println(commons.GetSetString("test","newTest"))
+
+	//fmt.Println(client.StrLen("test"))
+
+	//fmt.Println(client.Append("test2","121212").Result())
+
+	//fmt.Println(client.HSet("hset","test","wer3232wew").Result())
+	//commons.HSet("hset","test1","速度速度速度")
+
+	//fmt.Println(client.HSetNX("hset","test2","121h23hh23hh2h32").Result())
+	//commons.HSetNX("hset","test3","1111111111")
+
+	//fmt.Println(commons.HGet("hset","test3"))
+
+	//fmt.Println(client.HExists("hset","test").Result())
+	//fmt.Println(commons.HExists("hset","test"))
+
+	//fmt.Println(client.HDel("hset","test","test1").Result())
+	//commons.HDel("hset","test2","test3")
 }
--- commons/RedisUtil.go
+++ commons/RedisUtil.go
@@ -4,9 +4,11 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/lizhixin1992/test/conf"
 	"github.com/pelletier/go-toml"
+	"log"
+	"time"
 )
 
-var Cache = New()
+var redisClient = New()
 
 func New() *redis.Client {
 	tree := conf.GlobalConf.Get("redis").(*toml.Tree)
@@ -15,4 +17,109 @@ func New() *redis.Client {
 		Password: "",
 		DB:       int(tree.Get("DB").(int64)),
 	})
+}
+
+func SetString(key, value string, time time.Duration) {
+	err := redisClient.Set(key, value, time).Err()
+	if err != nil {
+		log.Fatal("redis SetString is err, err : ", err)
+		//panic(err)
+	}
+}
+
+//只在键 key 不存在的情况下， 将键 key 的值设置为 value
+//若键 key 已经存在， 则 SETNX 命令不做任何动作
+func SetNXString(key, value string, time time.Duration) (flag bool) {
+	flag, err := redisClient.SetNX(key, value, time).Result()
+	if err != nil {
+		flag = false
+		log.Fatal("redis SetNXString is err, err : ", err)
+		//panic(err)
+	}
+	return flag
+}
+
+//和SetNX相反，只在键已经存在时， 才对键进行设置操作
+func SetXXString(key, value string, time time.Duration) (flag bool) {
+	flag, err := redisClient.SetXX(key, value, time).Result()
+	if err != nil {
+		flag = false
+		log.Fatal("redis SetXXString is err, err : ", err)
+		//panic(err)
+	}
+	return flag
+}
+
+func GetString(key string) (value string) {
+	value, err := redisClient.Get(key).Result()
+	if err != nil {
+		log.Fatal("redis GetString is err, err : ", err)
+		//panic(err)
+	}
+	return value
+}
+
+//将键 key 的值设为 value ， 并返回键 key 在被设置之前的旧值
+//如果键 key 没有旧值， 也即是说， 键 key 在被设置之前并不存在， 那么命令返回 nil,并且按照传入的key/value保存到redis中
+func GetSetString(kev, value string) (oldValue string) {
+	oldValue, err := redisClient.GetSet(kev, value).Result()
+	if err != nil {
+		log.Fatal("redis GetString is err, err : ", err)
+	}
+	return oldValue
+}
+
+//如果键 key 已经存在并且它的值是一个字符串， APPEND 命令将把 value 追加到键 key 现有值的末尾
+//如果 key 不存在， APPEND 就简单地将键 key 的值设为 value ， 就像执行 SET key value 一样
+func AppendString(key, value string) {
+	_, err := redisClient.Append(key, value).Result()
+	if err != nil {
+		log.Fatal("redis AppendString is err, err : ", err)
+	}
+}
+
+//将哈希表 hash 中域 field 的值设置为 value
+func HSet(key, field string, value interface{}) {
+	_, err := redisClient.HSet(key, field, value).Result()
+	if err != nil {
+		log.Fatal("redis HSet is err, err : ", err)
+	}
+}
+
+//当且仅当域 field 尚未存在于哈希表的情况下， 将它的值设置为 value
+//如果给定域已经存在于哈希表当中， 那么命令将放弃执行设置操作
+//如果哈希表 hash 不存在， 那么一个新的哈希表将被创建并执行 HSETNX 命令
+func HSetNX(key, field string, value interface{}) {
+	_, err := redisClient.HSetNX(key, field, value).Result()
+	if err != nil {
+		log.Fatal("redis HSetNX is err, err : ", err)
+	}
+}
+
+//返回哈希表中给定域的值
+func HGet(key, field string) (value string) {
+	value, err := redisClient.HGet(key, field).Result()
+	if err != nil {
+		log.Fatal("redis HGet is err, err : ", err)
+	}
+	return value
+}
+
+//检查给定域 field 是否存在于哈希表 hash 当中
+func HExists(key, field string) (flag bool) {
+	flag, err := redisClient.HExists(key, field).Result()
+	if err != nil {
+		log.Fatal("redis HExists is err, err : ", err)
+	}
+	return flag
+}
+
+//删除哈希表 key 中的一个或多个指定域，不存在的域将被忽略
+func HDel(key string, fields ...string) {
+	for _, value := range fields {
+		_, err := redisClient.HDel(key, value).Result()
+		if err != nil {
+			log.Fatal("redis HDel is err, err : ", err)
+		}
+	}
 }
