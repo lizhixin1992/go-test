aebc2a9132d4a188075df48907fd6637daaef772
lizhixin <lzx920911@gmail.com>
2019-06-06 14:39:08 +0800

    commit

--- commons/RedisUtil.go
+++ commons/RedisUtil.go
@@ -695,3 +695,19 @@ func DBSize() (value int64) {
 	}
 	return value
 }
+
+//查找所有符合给定模式 pattern 的 key ， 比如说：
+//KEYS * 匹配数据库中所有 key 。
+//KEYS h?llo 匹配 hello ， hallo 和 hxllo 等。
+//KEYS h*llo 匹配 hllo 和 heeeeello 等。
+//KEYS h[ae]llo 匹配 hello 和 hallo ，但不匹配 hillo 。
+//特殊符号用 \ 隔开
+//
+//KEYS 的速度非常快，但在一个大的数据库中使用它仍然可能造成性能问题，如果你需要从一个数据集中查找特定的 key ，你最好还是用 Redis 的集合结构(set)来代替
+func Keys(pattern string) (value []string) {
+	value, err := redisClient.Keys(pattern).Result()
+	if err != nil {
+		log.Fatal("redis Keys is err, err : ", err)
+	}
+	return value
+}
