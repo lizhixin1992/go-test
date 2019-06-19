b47df599abd81baca37592cbf3acaf8be0487463
lizhixin <lzx920911@gmail.com>
2019-06-19 17:01:55 +0800

    commit

--- _demo/elastic/elasticTest.go
+++ _demo/elastic/elasticTest.go
@@ -1,9 +1,110 @@
-package elastic
+package main
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
 	elasticsearch6 "github.com/elastic/go-elasticsearch/v6"
+	"log"
+	"strings"
+	//"sync"
 )
 
 func main() {
+	log.SetFlags(0)
 
+	var (
+		r map[string]interface{}
+		//wg sync.WaitGroup
+	)
+
+	cof := elasticsearch6.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+		},
+	}
+	es, err := elasticsearch6.NewClient(cof)
+
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	// 1. Get cluster info
+	//
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	// Check response status
+	if res.IsError() {
+		log.Fatalf("Error: %s", res.String())
+	}
+	// Deserialize the response into a map.
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+	// Print client and server version numbers.
+	log.Printf("Client: %s", elasticsearch6.Version)
+	log.Printf("Server: %s", r["version"].(map[string]interface{})["number"])
+	log.Println(strings.Repeat("~", 37))
+
+	// 2.Search for the indexed documents
+	//
+	var buf bytes.Buffer
+
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"passage": "elk rocks",
+			},
+		},
+	}
+
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		log.Fatalf("Error encoding query: %s", err)
+	}
+
+	res, err = es.Search(
+		es.Search.WithContext(context.Background()),
+		es.Search.WithIndex("book1"),
+		es.Search.WithBody(&buf),
+		es.Search.WithTrackTotalHits(true),
+		es.Search.WithPretty(),
+	)
+
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		var e map[string]interface{}
+		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
+			log.Fatalf("Error parsing the response body: %s", err)
+		} else {
+			// Print the response status and error information.
+			log.Fatalf("[%s] %s: %s",
+				res.Status(),
+				e["error"].(map[string]interface{})["type"],
+				e["error"].(map[string]interface{})["reason"],
+			)
+		}
+	}
+
+	if err := json.NewDecoder(res.Body).Decode(&r); err != nil {
+		log.Fatalf("Error parsing the response body: %s", err)
+	}
+
+	log.Printf(
+		"[%s] %d hits; took: %dms",
+		res.Status(),
+		int(r["hits"].(map[string]interface{})["total"].(float64)),
+		int(r["took"].(float64)),
+	)
+
+	for _, hit := range r["hits"].(map[string]interface{})["hits"].([]interface{}) {
+		log.Printf(" * ID=%s, %s", hit.(map[string]interface{})["_id"], hit.(map[string]interface{})["_source"])
+	}
+
+	log.Println(strings.Repeat("=", 37))
 }
